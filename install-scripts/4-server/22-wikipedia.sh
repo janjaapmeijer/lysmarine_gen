a377cecd223a42ccfca20299ce223f7e5d443ac1
Jan Jaap Meijer <janjaapmeijer@users.noreply.github.com>
2024-04-15 13:42:35 +1000

    Update 22-wikipedia.sh for rockpi.

--- install-scripts/4-server/22-wikipedia.sh
+++ install-scripts/4-server/22-wikipedia.sh
@@ -4,4 +4,4 @@ if [ "$BBN_KIND" == "LITE" ] ; then
   exit 0
 fi
 
-apt-get install -y liblzma5 libicu72 libzstd1 libxapian30 libcurl3-gnutls aria2 libkiwix11 kiwix-tools
+apt-get install -y liblzma5 libicu72 libzstd1 libxapian30 libcurl3-gnutls libpugixml1v5aria2 libkiwix11 kiwix-tools
