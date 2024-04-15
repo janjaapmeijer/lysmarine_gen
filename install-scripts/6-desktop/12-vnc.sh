43f7c7722994197ca2668c57c48d8d9c6a5a4e3f
Jan Jaap Meijer <janjaapmeijer@users.noreply.github.com>
2024-04-15 13:49:31 +1000

    Update 12-vnc.sh for rockpi.

--- install-scripts/6-desktop/12-vnc.sh
+++ install-scripts/6-desktop/12-vnc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
-apt-get install -y -q realvnc-vnc-viewer
+# apt-get install -y -q realvnc-vnc-viewer
 
+#wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.5.1-Linux-ARM64.deb
+#dpkg -i VNC-Server-*-ARM64.deb
+#rm VNC-Server-*.deb
 
+#systemctl enable vncserver-x11-serviced.service
