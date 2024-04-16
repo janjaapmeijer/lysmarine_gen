5118d58488d8d36625b8c0d85d40b5904ad30f39
Jan Jaap Meijer <janjaapmeijer@users.noreply.github.com>
2024-04-16 18:27:29 +1000

    Add if service active statements.

--- install-scripts/2-baseSystem/02-network.sh
+++ install-scripts/2-baseSystem/02-network.sh
@@ -7,25 +7,31 @@ apt-get install -y -q network-manager make avahi-daemon bridge-utils wakeonlan #
 install -v "$FILE_FOLDER"/hostname "/etc/"
 cat "$FILE_FOLDER"/hosts >> /etc/hosts
 if [ $thisArch == "armbian"]; then
-  sed -i "/${hostname,}/d" /etc/hosts
+  sed -i '/"${hostname,}"/d' /etc/hosts
 else
   sed -i '/raspberrypi/d' /etc/hosts
 fi
 
 # Access Point management
 install -m0600 -v "$FILE_FOLDER"/lysmarine-hotspot.nmconnection "/etc/NetworkManager/system-connections/"
-systemctl disable dnsmasq || true
+if service --status-all | grep -Fq 'dnsmasq'; then
+  systemctl disable dnsmasq || true
+fi
 
 ##  NetworkManager provide its own wpa_supplicant, stop the others to avoid conflicts.
 if service --status-all | grep -Fq 'dhcpcd'; then
-	systemctl disable dhcpcd.service
+  systemctl disable dhcpcd.service
 fi
 systemctl disable wpa_supplicant.service
-systemctl disable hostapd.service || true
+if service --status-all | grep -Fq 'hostapd'; then
+  systemctl disable hostapd.service || true
+fi
 
 ## Disable some useless networking services
 systemctl disable NetworkManager-wait-online.service # if we do not boot remote user over the network this is not needed
-systemctl disable ModemManager.service # for 2G/3G/4G
+if service --status-all | grep -Fq 'ModemManager'; then
+  systemctl disable ModemManager.service # for 2G/3G/4G
+fi
 systemctl disable pppd-dns.service || true # For dial-up Internet LOL
 
 install -v -m 0644 "$FILE_FOLDER"/wifi_powersave@.service "/etc/systemd/system/"
